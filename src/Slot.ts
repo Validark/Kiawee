750af071d232beb0140bd515b455e6e6e68ab6b8
Xuleos <Evancat77@gmail.com>
2020-03-16 19:06:31 -0400

    Calculate initial entropy for slot

--- src/Slot.ts
+++ src/Slot.ts
@@ -1,4 +1,19 @@
 import { Tile } from "./Tile";
 export class Slot {
-	constructor(public pos: Vector3, public tiles: Array<Tile>) {}
+	Entropy: number;
+	constructor(public pos: Vector3, public tiles: Array<Tile>) {
+		this.Entropy = this.CalculateEntropy();
+	}
+
+	CalculateEntropy() {
+		const totalProbability = this.tiles.reduce((accumulator, tile) => {
+			return accumulator + tile.probability;
+		}, 0);
+
+		const totalPLogP = this.tiles.reduce((accumulator, tile) => {
+			return accumulator + tile.pLogP;
+		}, 0);
+
+		return (1 / totalProbability) * totalPLogP + math.log(totalProbability);
+	}
 }
