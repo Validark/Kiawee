1fcba77327290605e94f5a6455c906e42f64c853
Xuleos <Evancat77@gmail.com>
2020-03-17 02:37:22 -0400

    make AvailableModuleTotal camelcase

--- src/Propagator.ts
+++ src/Propagator.ts
@@ -14,15 +14,15 @@ export class Propagator {
 
 	Run() {
 		// we gotta test this
-		const AvailableModuleTotal = this.slots.reduce((total, slot) => {
+		const availableModuleTotal = this.slots.reduce((total, slot) => {
 			return total - slot.tiles.size();
 		}, 0);
 
 		if (this.options.Debug) {
-			print(`${AvailableModuleTotal} modules left in wave function`);
+			print(`${availableModuleTotal} modules left in wave function`);
 		}
 
-		while (AvailableModuleTotal > 0) {
+		while (availableModuleTotal > 0) {
 			//this could be a bad idea in the future and result in infinite yielding
 		}
 
