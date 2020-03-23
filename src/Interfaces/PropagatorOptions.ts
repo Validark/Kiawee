cc5bb9866f1ee501ec9fe50dc1c71f0e94b8f7f1
Xuleos <Evancat77@gmail.com>
2020-03-23 01:46:42 -0400

    Remove options that do nothing

--- src/Interfaces/PropagatorOptions.ts
+++ src/Interfaces/PropagatorOptions.ts
@@ -1,9 +1,5 @@
-import { BaseConstraint } from "../Constraints/BaseConstraint";
-
 interface PropagatorOptions {
-	Constraints?: Array<BaseConstraint>;
 	Seed?: number;
-	BackTraceLimit?: 0 | -1 | number;
 	Debug?: boolean;
 }
 
